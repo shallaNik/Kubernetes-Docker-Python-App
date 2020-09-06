3a9fc3cf749b2707345036da22cec9372043c712
shallaNik <43818681+shallaNik@users.noreply.github.com>
2020-09-06 11:56:59 +0530

    initial commit

---
+++ game.py
@@ -0,0 +1,22 @@
+# Python program to shuffle a deck of card
+
+# importing modules
+import itertools, random
+
+# make a deck of cards... itertools.product cartesian product from given range with given list of cards item 
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+
+# shuffle the cards
+random.shuffle(deck)
+
+# each player draws 13 cards
+print("----------------GAME BEGINS------------------")
+print("******PLAYER 1******")
+print("You got:")
+for i in range(13):
+   print(deck[i][0], "of", deck[i][1])
+   
+print("******PLAYER 2******")
+print("You got:")
+for i in range(13):
+   print(deck[i][0], "of", deck[i][1])
